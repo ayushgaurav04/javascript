e67738a8d11d6184c78b930bfab5c725aba3f687
ayushgaurav04 <itsaghzb@gmail.com>
2024-06-17 00:40:32 +0530

    second comit

---
+++ 02_data_types/01.js
@@ -0,0 +1,17 @@
+// in this we are going to see the datatypes they are the object, bolean , strings , null
+// stings--> "Ayush"
+// bolean--> true , false
+// object --> null
+// undefined --> undefined
+// -------------------------------------------------------------------------
+console.log("I am the datatype");
+let names = "ayush"
+let password = 1234
+let ayush = true
+let gaurav = null// it is the object this is askefd in the various interviewes
+let rahul;
+console.log(typeof(names))
+console.log(typeof(password))
+console.log(typeof(ayush))
+console.log(typeof(gaurav))
+console.log(typeof(rahul))
