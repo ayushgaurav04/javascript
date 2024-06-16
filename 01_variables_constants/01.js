400d774fa10d557a947e17d60eea0fe09925af62
ayushgaurav04 <itsaghzb@gmail.com>
2024-06-17 00:06:18 +0530

    first commit

---
+++ 01_variables_constants/01.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+var accountname = "Ayush"
+let password = 1234
+homecity = "Bhopal"
+let gaurav;//this will give the undefined output
+
+// changing the above value to see the result of the programming..
+// accountId = 2354
+accountname = "Gaurav"
+password = 3254
+homecity = "Hazaribagh"
+//this is to print the program
+console.log( accountId, accountname, password, homecity);
+console.table( [accountId, accountname, password, homecity , gaurav]);
