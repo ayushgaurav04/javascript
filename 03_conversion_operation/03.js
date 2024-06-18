1ebd57b66426f2ad580a048580d7aac76a086674
Ayush Gaurav <163287937+ayushgaurav04@users.noreply.github.com>
2024-06-19 01:29:38 +0530

    day3

---
+++ 03_conversion_operation/03.js
@@ -0,0 +1,17 @@
+// this is all about the confusion created on the java script while converting the code 
+console.log(" I am the confusion. ")
+let num1= 1
+let num2= 2
+console.log(num1+num2)
+// -------------------------------------------------------------------
+// other example
+console.log(1+2)
+console.log("1"+3)//string me output 13 aya hai.
+console.log(1+2+"3")// first 1and 2 will be added then it wil  combine with 3
+console.log (true)
+let user="ayush"
+let user2 = Boolean(user)
+console.log(user2)
+let valuescore=6000
+++valuescore;// 6000 me 1 incriment ho jayega.
+console.log(valuescore)
