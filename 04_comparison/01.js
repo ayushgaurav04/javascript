267b47833a55cb6e328a889fae2426e230ed49fa
Ayush Gaurav <163287937+ayushgaurav04@users.noreply.github.com>
2024-06-28 11:55:47 +0530

    comparision

---
+++ 04_comparison/01.js
@@ -0,0 +1,13 @@
+// in this we are going to learn the comparision of the variables.
+console.log (" The easy program")
+let a = 1 , b=2;
+greater = a<b;
+console.log(greater);
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)// null equal to zero ho jata hai.
+console.log("I am undefined from here")
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
+console.log(undefined<0)
